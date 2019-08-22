861f2346b7a784b453b2cc4f5c68addeec06b97b
韦宏毅 <weihongyi@syswin.com>
2019-08-22 16:31:50 +0800

    146

--- src/n0146.rs
+++ src/n0146.rs
@@ -1,106 +1,133 @@
 
 use std::collections::HashMap;
+use std::ptr;
+use std::mem;
 
-#[derive(Clone, Debug)]
-struct LRUCache {
-    capacity: usize,
-    head: Option<Box<Node>>,
-    tail: Option<Box<Node>>,
-    map: HashMap<i32, Box<Node>>,
+// Entry is either a map entry and a link-list node
+pub struct LRUEntry {
+    key: i32,
+    val: i32,
+    prev: *mut LRUEntry,
+    next: *mut LRUEntry,
 }
 
+impl LRUEntry {
+    pub fn new(key: i32, val: i32) -> Self {
+        LRUEntry{
+            key: key,
+            val: val,
+            prev: ptr::null_mut(),
+            next: ptr::null_mut(),
+        }
+    }
+}
 
-/**
- * `&self` means the method takes an immutable reference.
- * If you need a mutable reference, change it to `&mut self` instead.
- */
+pub struct LRUCache {
+    map: HashMap<i32, Box<LRUEntry>>,
+    cap: usize,
+
+    // head and tail is dummy node of the double-linked-list
+    head: *mut LRUEntry,
+    tail: *mut LRUEntry,
+}
+
 impl LRUCache {
-
-    fn new(capacity: i32) -> Self {
-        let cache = LRUCache {
-            capacity: capacity as usize,
-            head: None,
-            tail: None,
-            map: HashMap::with_capacity(capacity as usize)
+    pub fn new(capacity: i32) -> Self {
+        let capacity = capacity as usize;
+        let map = HashMap::with_capacity(capacity);
+        let cache = LRUCache{
+            map: map,
+            cap: capacity,
+            head: unsafe { Box::into_raw(Box::new(mem::uninitialized::<LRUEntry>())) },
+            tail: unsafe { Box::into_raw(Box::new(mem::uninitialized::<LRUEntry>())) },
         };
+        unsafe {
+            (*cache.head).next = cache.tail;
+            (*cache.tail).prev = cache.head;
+        }
+
         cache
     }
 
-    fn get(&mut self, key: i32) -> i32 {
-         let v = match self.map.get_mut(&key) {
-            Some(node) => {
-                self.detach(node);
-                self.push(node);
-                node.value
-            }
-            None => {
-                -1
+    pub fn get(&mut self, key: i32) -> i32 {
+        let (ptr, val) = match self.map.get_mut(&key) {
+            None => (None, None),
+            Some(entry) => {
+                let ptr: *mut LRUEntry = &mut **entry;
+                (Some(ptr), Some(unsafe { (*entry).val }))
             }
         };
-        v
+
+        if let Some(ptr) = ptr {
+            self.detach(ptr);
+            self.push(ptr);
+        }
+        val.unwrap_or(-1)
     }
 
-    fn put(&mut self, key: i32, value: i32) {
-        let node = self.map.get_mut(&key);
+    pub fn put(&mut self, key: i32, value: i32) {
+        let ptr = self.map.get_mut(&key).map(|entry| {
+            let ptr: *mut LRUEntry = &mut **entry;
+            ptr
+        });
 
-        match node {
-            //不存在则添加
+        match ptr {
+            Some(ptr) => {
+                // key already exist, update it
+                unsafe { (*ptr).val = value };
+                self.detach(ptr);
+                self.push(ptr);
+            }
             None => {
-                //如果满了，需要pop一个
-                if self.map.len() == self.capacity {
-                    self.pop();
+                // insert new key, cache is full, evict
+                if self.map.len() == self.cap {
+                    let mut old_entry = self.pop().unwrap();
+                    old_entry.key = key;
+                    old_entry.val = value;
+                    self.push(&mut *old_entry);
+                    self.map.insert(key, old_entry);
+                } else {
+                    let mut new_entry = Box::new(LRUEntry::new(key, value));
+                    self.push(&mut *new_entry);
+                    self.map.insert(key, new_entry);
                 }
-                let mut new_node = Box::new(Node::new(key, value));
-                self.push(new_node);
-                self.map.insert(key, new_node );
             }
-            Some(n) =>{
-                self.detach(n);
-                self.push(n);
-            }
-
         }
     }
-    // pop() remove the head entry from map, detach the entry from head of linked-list, and return it
-    fn pop(&mut self) -> Option<Box<Node>> {
-        let mut first = self.head;
-//        if first == None {
-//            return None;
-//        }
 
-        self.map.remove(&first.unwrap().key);
-        self.detach(first.unwrap());
-        first
+    // pop() remove the entry from map, detach the entry from head of linked-list, and return it
+    fn pop(&mut self) -> Option<Box<LRUEntry>> {
+        let next;
+        unsafe { next = (*self.head).next }
+        // list is empty
+        if next == self.tail {
+            return None
+        }
+        let key = unsafe { (*next).key };
+        let mut old_entry = self.map.remove(&key).unwrap();
+        self.detach(&mut *old_entry);
+        Some(old_entry)
+    }
 
-    }
-    // detach() remove an entry from the linked-list
-    fn detach(&mut self, mut node: Box<Node>) {
-        node.pre.unwrap().next = node.next;
-        node.next.unwrap().pre = node.pre;
-    }
     // push() pushs an entry to the tail of linked-list
-    fn push(&mut self, mut node:  Box<Node>) {
-        if self.head == None {
-            self.head = Some(node);
+    fn push(&mut self, entry: *mut LRUEntry) {
+        unsafe {
+            // prev <-> tail
+            // prev <-> entry <-> tail
+            (*entry).prev = (*self.tail).prev;
+            (*entry).next = self.tail;
+            (*self.tail).prev = entry;
+            (*(*entry).prev).next = entry;
         }
-
     }
 
-}
-#[derive(Clone, Debug)]
-struct Node {
-    pre: Option<Box<Node>>,
-    next: Option<Box<Node>>,
-    key: i32,
-    value: i32,
-}
-impl Node {
-    fn new(key: i32, val: i32) -> Self {
-        Node{
-            key,
-            value: val,
-            pre: None,
-            next: None,
+    // detach() remove an entry from the linked-list
+    fn detach(&mut self, entry: *mut LRUEntry) {
+        unsafe {
+            // prev <-> entry <-> next
+            // prev <-> next
+            (*(*entry).prev).next = (*entry).next;
+            (*(*entry).next).prev = (*entry).prev;
         }
     }
 }
